e2e0f3c095d6c8d7b99496ff0ca86d93f3b43e55
SOLO-Trippy <neificastillo400@gmail.com>
2026-04-15 17:37:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-node_modules/
-.env
-*.log
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("A player joined");
+
+  socket.on("disconnect", () => {
+    console.log("A player left");
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
